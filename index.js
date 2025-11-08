d61efcbd0e7631e89259d4a7268defc67c7013c6
MohmedFahmy <mohammedfahmy130@gmail.com>
2025-11-08 14:41:53 +0200

    fenishing the apis and the midelwares

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const coursesRoutes = require('./routes/courses_routes');
+const mongoose = require('mongoose');
+const uri ='mongodb+srv://mohammedfahmy130_db_user:mohamedFahmy151@courses.b1vjarr.mongodb.net/?appName=courses';
+mongoose.connect(uri)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
+
 app.use(express.json());
 
 app.use('/api/courses',coursesRoutes);
