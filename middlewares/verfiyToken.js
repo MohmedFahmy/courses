9d578682385d220d89ca3d22f2ffb72d6e16c674
MohmedFahmy <mohammedfahmy130@gmail.com>
2025-12-14 20:53:11 +0200

    feat: enhance token verification middleware with error handling for missing and invalid tokens

--- middlewares/verfiyToken.js
+++ middlewares/verfiyToken.js
@@ -1,8 +1,20 @@
 
-
+const jwt = require('jsonwebtoken');
+const app_errors = require('../utils/app_errors');
+const http_status_text = require('../utils/http_status_text');
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'] || req.headers['Authorization'];
+    if(!authHeader ) {
+        const error = app_errors.createError('No token provided', 401, http_status_text.ERROR);
+        return next(error);
+    }
     const token = authHeader.split(' ')[1];
-    console.log("Verifying token:", token);
-    next();
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        next();
+    } catch (err) {
+        const error = app_errors.createError('Invalid token', 401, http_status_text.ERROR);
+                return next(error);
+    }
 };
+module.exports = verifyToken;
